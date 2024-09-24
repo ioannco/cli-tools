f4e8e75e85a0b8b94b3145fea6453448989634d8
ioannco <cheremisenov96@gmail.com>
2024-09-24 15:07:06 +0300

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 function install_and_shorten {
 echo "#!/usr/bin/env python3" > ./bin/$1
 cat ./src/$1.py >> ./bin/$1
@@ -13,4 +14,4 @@ install_and_shorten browse b
 install_and_shorten google g
 install_and_shorten svace-issue svi
 
-echo Installation completed. To use tools from anywhere, please add \"$(pwd)\" to the PATH.
+echo Installation completed. To use tools from anywhere, please add \"$(pwd)/bin\" to the PATH.
