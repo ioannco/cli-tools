66b280a258c256bb8a808ea59a577f626f19c33d
ioannco <cheremisenov96@gmail.com>
2024-09-24 15:04:42 +0300

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 function install_and_shorten {
-echo "#!/usr/bin/env python" > bin/$2
-cat src/$1.py >> bin/$1
-chmod +x bin/$1
-ln -s bin/$1 bin/$2
+echo "#!/usr/bin/env python3" > ./bin/$1
+cat ./src/$1.py >> ./bin/$1
+cd bin
+chmod +x $1
+ln -s $1 $2
+cd ..
 }
 
-if [ -d bin ]; then rm -Rf bin; fi
-
+if [ -d ./bin ]; then rm -Rf ./bin; fi
+mkdir ./bin
 install_and_shorten browse b
 install_and_shorten google g
 install_and_shorten svace-issue svi
